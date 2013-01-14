0877ea689d132de5c715138bc8705d71e1fcc279
Paul Draper <paulddraper@gmail.com>
2013-01-14 11:48:51 -0700

    Added 'Make admin' action to User admin

--- acm-django/membership/admin.py
+++ acm-django/membership/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group, User
 from membership.models import *
 
 class MemberInline(admin.StackedInline):
@@ -7,22 +8,17 @@ class MemberInline(admin.StackedInline):
 	max_num = 1
 	can_delete = False
 
+def make_admin(modeladmin, request, queryset):
+	admin_group = Group.objects.get(name='Administrator')
+	for user in queryset:
+		user.is_staff = True
+		user.groups.add(admin_group)
+		user.save()
+make_admin.short_description = 'Make selected users Administrators'
+
 class MyUserAdmin(UserAdmin):
 	inlines = [MemberInline]
-
-class MyModelAdmin(admin.ModelAdmin):
-
-	def changelist_view(self, request, extra_context=None):
-		print 'change'
-		return HttpResponse()
-
-	def add_view(self, request, form_url='', extra_context=None):
-		print 'view'
-		return HttpResponse()
-
-	def change_view(self, request, object_id, extra_context=None):
-		print 'change'
-		return HttpResponse()
+	actions = [make_admin,]
 
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)

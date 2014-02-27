9482e160fd2a39d5df6dd8242f69db3b3a2c879e
Paul Draper <paulddraper@gmail.com>
2014-02-26 19:44:23 -0700

    Fix Django admin by enabling messages middleware

--- acm-django/app/acm/settings.py
+++ acm-django/app/acm/settings.py
@@ -104,7 +104,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -126,7 +126,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
 #    'django.contrib.sites',
-#    'django.contrib.messages',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.forms',
     'django.contrib.admin',

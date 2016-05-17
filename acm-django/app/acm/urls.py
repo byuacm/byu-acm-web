148774bc89c19d23fbe0756f69ea6d549977242e
Derek Argueta <darguetap@gmail.com>
2016-05-17 01:34:07 -0600

    reduce warnings

--- acm-django/app/acm/urls.py
+++ acm-django/app/acm/urls.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.conf.urls import include, patterns, url
 from django.contrib import admin
+from django.contrib.auth.views import login, password_reset_complete, password_reset_confirm, password_change, password_reset
 from django.core.urlresolvers import reverse_lazy
 from util.views import redirect_view
+
+from membership.views import logout
 
 admin.autodiscover()
 
@@ -14,16 +17,16 @@ urlpatterns = patterns('',
 
     # Login accounts
     url(r'^$',redirect_view('membership.views.enrollment')),
-    url(r'^accounts/login/', 'django.contrib.auth.views.login'),
-    url(r'^accounts/logout/', 'membership.views.logout'),
-    url(r'^accounts/password_change/', 'django.contrib.auth.views.password_change', {
+    url(r'^accounts/login/', login),
+    url(r'^accounts/logout/', logout),
+    url(r'^accounts/password_change/', password_change, {
         'post_change_redirect': 'django.contrib.auth.views.login',
     }),
-    url(r'^accounts/password_reset_confirm/(?P<uidb36>\w+)-(?P<token>\w+-\w+)/$', 'django.contrib.auth.views.password_reset_confirm'),
-    url(r'^accounts/password_reset/', 'django.contrib.auth.views.password_reset', {
+    url(r'^accounts/password_reset_confirm/(?P<uidb36>\w+)-(?P<token>\w+-\w+)/$', password_reset_confirm),
+    url(r'^accounts/password_reset/', password_reset, {
         'post_reset_redirect': reverse_lazy('membership.views.edit_member'),
     }),
-    url(r'^accounts/password_reset_complete/', 'django.contrib.auth.views.password_reset_complete'),
+    url(r'^accounts/password_reset_complete/', password_reset_complete),
 
     # Membership
     url(r'^membership/', include('membership.urls')),
--- acm-django/app/dashboard/urls.py
+++ acm-django/app/dashboard/urls.py
@@ -1,16 +1,18 @@
 from django.conf.urls import patterns, url
 
+from views import make_member_list, raffle, attendance, shirt_sizes, member_list, points
+
 urlpatterns = patterns(
     'dashboard.views',
-    url(r'^$', 'make_member_list'),
-    url(r'^raffle/$', 'raffle'),
+    url(r'^$', make_member_list),
+    url(r'^raffle/$', raffle),
     url(r'^raffle/(?P<meeting_pk>\d+)/$', 'raffle'),
-    url(r'^attendance/$', 'attendance'),
+    url(r'^attendance/$', attendance),
     url(r'^attendance/(?P<meeting_pk>\d+)/$', 'attendance'),
-    url(r'^shirt_sizes/$', 'shirt_sizes'),
+    url(r'^shirt_sizes/$', shirt_sizes),
     url(r'^shirt_sizes/(?P<semester_pk>\d+)/$', 'shirt_sizes'),
-    url(r'^member_list/create/$', 'make_member_list'),
-    url(r'^member_list/$', 'member_list'),
+    url(r'^member_list/create/$', make_member_list),
+    url(r'^member_list/$', member_list),
     url(r'^member_list/(?P<semester_pk>\d+)/$', 'member_list'),
-    url(r'^points/(?P<meeting_pk>\d+/)?$', 'points'),
+    url(r'^points/(?P<meeting_pk>\d+/)?$', points),
 )

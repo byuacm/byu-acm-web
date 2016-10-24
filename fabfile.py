3afb26f6d677beed5457cfb3ee407ddbf0aed9ba
hcodyd <cody.d.h.harrison@gmail.com>
2016-10-24 18:48:54 -0400

    added fab file to ease deployment, secrets.py updated aswell

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+### easy commands####
+from secrets import password
+from fabric.api import local,env,run,cd
+env.password=password
+env.user= 'acm'
+env.hosts= [
+	'acm.byu.edu'
+	]
+def host_type():
+	run('uname -s')
+
+
+def commit():
+	local("git add -p && git commit")
+
+def push():
+	local("git push")
+
+def prepare_deploy():
+	commit()
+	push()
+
+
+def deploy():
+	with cd('byu-acm-web'):
+		run('git pull derek master')
+		run('make deploy-public')
+	

16b867faaa408e1fe8eb93493c126d65bdb1dcf5
Derek Argueta <darguetap@gmail.com>
2016-10-26 06:54:04 -0600

    formatting for fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,28 +1,33 @@
-### easy commands####
 from secrets import password
-from fabric.api import local,env,run,cd
-env.password=password
-env.user= 'acm'
-env.hosts= [
-	'acm.byu.edu'
-	]
+from fabric.api import local, env, run, cd
+
+
+env.password = password
+env.user = 'acm'
+env.hosts = [
+    'acm.byu.edu'
+]
+
+
 def host_type():
-	run('uname -s')
+    run('uname -s')
 
 
 def commit():
-	local("git add -p && git commit")
+    local("git add -p && git commit")
+
 
 def push():
-	local("git push")
+    local("git push")
+
 
 def prepare_deploy():
-	commit()
-	push()
+    commit()
+    push()
 
 
 def deploy():
-	with cd('byu-acm-web'):
-		run('git pull derek master')
-		run('sudo make deploy-public')
-	
+    with cd('byu-acm-web'):
+        run('git pull derek master')
+        run('sudo make deploy-public')
+

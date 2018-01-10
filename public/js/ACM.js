7cbc540318d74f2a27f1922cc9acbde79526062f
Alyssa Haroldsen <kupiakos@gmail.com>
2018-01-09 21:32:47 -0700

    Update events to load from a JSON

--- public/js/ACM.js
+++ public/js/ACM.js
@@ -21,29 +21,8 @@ app.config(['$routeProvider', function($routeProvider,$locationProvider) {
 //  ROOTSCOPE SETUP
 //*****************************************************************************
 app.run(function($rootScope) {
-
-  // NavBar & Page Title (for highlighting currently selected)
-  $rootScope.slideshow;
-  $rootScope.setSlideshow = function(show) {
-    $rootScope.slideshow = show;
-  }
-  $rootScope.clearSlideshow = function() {
-    clearTimeout($rootScope.slideshow);
-  }
-
-    $rootScope.leaders = [];
-  $rootScope.hasList = false;
-  $rootScope.setLeaders = function(list) {
-        $rootScope.leaders = list;
-    $rootScope.hasList = true;
-  }
-  $rootScope.getLeaders = function() {
-    return $rootScope.leaders;
-  }
-  $rootScope.hasLeaderList = function() {
-    return $rootScope.hasList;
-  }
-
+  $rootScope.leaders = [];
+  $rootScope.events = [];
 });
 
 //*****************************************************************************
@@ -52,50 +31,6 @@ app.run(function($rootScope) {
 
 function HomeCtrl($scope, $routeParams) {
 
-  // slideshow
-  // 45 X 13 aspect ratio
-  // $scope.photos = [
-  //   { url: 'img/misc/group-chat-2.jpg' },
-  //   { url: 'img/meetings/opening_social_winter_2013-1-cropped.jpg' },
-  //   { url: 'img/misc/prize-winner-2.jpg' },
-  //   { url: 'img/meetings/opening_social_winter_2013-4-cropped.jpg' },
-  //   { url: 'img/misc/coding-competition-W2015-2.jpg' }
-  // ];
-
-  // $scope.count = 0;
-  // $scope.total = $scope.photos.length;
-  // $scope.delay = 7500;
-  // $scope.photo = $scope.photos[$scope.count].url;
-
-  // $scope.changePhoto = function() {
-
-  //   $scope.count = ($scope.count + 1 == $scope.total) ? 0 : $scope.count + 1;
-
-  //   $('#photo-slide img').after('<img src="' + $scope.photos[$scope.count].url + '" class="above">');
-  //   $('#photo-slide img + img').animate({
-  //     bottom: '0px'
-  //   }, 500, function() {
-  //     $('#photo-slide').children().first().remove();
-  //     $(this).removeClass('above');
-  //   });
-    
-  //   $scope.setSlideshow(setTimeout(function() { $scope.changePhoto(); }, $scope.delay));
-  // }
-
-  // $scope.clickBar = function(direction) {
-
-  //   $scope.clearSlideshow();
-  //   if (direction == 'right') {
-  //     $scope.count = ($scope.count + 1 == $scope.total) ? 0 : $scope.count + 1;
-  //   } else {
-  //     $scope.count = ($scope.count - 1 < 0) ? $scope.total - 1 : $scope.count - 1;
-  //   }
-
-  //   $("#photo-slide img").attr('src', $scope.photos[$scope.count].url);
-  //   $scope.setSlideshow(setTimeout(function() { $scope.changePhoto(); }, $scope.delay));
-  // }
-
-  // $scope.setSlideshow(setTimeout(function() { $scope.changePhoto(); }, $scope.delay));
 }
 
 //*****************************************************************************
@@ -127,16 +62,12 @@ function LeaderCtrl($scope, $routeParams) {
     }, 500);
   }
   
-  $scope.leaders = [];
-  if (!$scope.hasLeaderList()) {
+  if ($scope.leaders.length === 0) {
     $.getJSON("js/officers.json").then(function(data) {
-        $scope.setLeaders(data);
-        $scope.leaders = $scope.getLeaders();
+        $scope.leaders = data;
         $scope.$digest();
       }
     );
-  } else {
-    $scope.leaders = $scope.getLeaders();
   }
 
 }
@@ -146,7 +77,13 @@ function LeaderCtrl($scope, $routeParams) {
 //*****************************************************************************
 
 function EventCtrl($scope, $routeParams) {
-
+  if ($scope.events.length === 0) {
+    $.getJSON("js/events.json").then(function(data) {
+        $scope.events = data;
+        $scope.$digest();
+      }
+    );
+  }
 }
 
 //*****************************************************************************
